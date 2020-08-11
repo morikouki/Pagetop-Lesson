61b7ea2910bdae84f37ee247b93dd0acaf378517
morikouki <akzouksia@icloud.com>
2020-08-11 13:03:05 +0900

    [Finish]JS-9

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 $(function() {
   $('#back a').on('click',function(event){
-    $('body, html').animate({
-      scrollTop:0
-    }, 1000);
-    event.preventDefault();
+  	$('body, html').animate({
+  		'scrollTop': 0
+  },800);
+  	event.preventDefault();
   });
 });
